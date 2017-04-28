c18422909dabdf16c3a661645e9fa613a9615c33
links234 <murtaza_alexandru73@yahoo.com>
2017-04-28 09:30:47 -0700

    Configurable ulimit settings and add systemd and pam.d configuration in core module

--- modules/core/scripts/install.sh
+++ modules/core/scripts/install.sh
@@ -3,22 +3,39 @@
 CORE_HOSTNAME=${1}
 GIT_USERNAME=${2}
 GIT_EMAIL=${3}
+GIT_URL=${4}
+
+ULIMIT_HARD=${5}
+ULIMIT_SOFT=${6}
+ULIMIT_SYSTEMD=${7}
 
 daedalus apt update
 
 daedalus apt dist-upgrade
 
-daedalus apply sysctl ${DAEDALUS_CONFIG_MODULE_PATH}/res/sysctl.conf
-daedalus apply security-limits ${DAEDALUS_CONFIG_MODULE_PATH}/res/security_limits.conf
-daedalus apply set-hostname $CORE_HOSTNAME
+daedalus template set ULIMIT_HARD "${ULIMIT_HARD}"
+daedalus template set ULIMIT_SOFT "${ULIMIT_SOFT}"
+daedalus template set ULIMIT_SYSTEMD "${ULIMIT_SYSTEMD}"
+daedalus tempalte render-to "${DAEDALUS_CONFIG_MODULE_PATH}/res/sysctl.conf" "${DAEDALUS_FULL_STATE_MODULE_PATH}/sysctl.conf"
+daedalus template render-to "${DAEDALUS_CONFIG_MODULE_PATH}/res/security_limits.conf" "${DAEDALUS_FULL_STATE_MODULE_PATH}/security_limits.conf"
+daedalus template render-to "${DAEDALUS_CONFIG_MODULE_PATH}/res/systemd.conf" "${DAEDALUS_FULL_STATE_MODULE_PATH}/systemd.conf"
+daedalus template render-to "${DAEDALUS_CONFIG_MODULE_PATH}/res/pam-common" "${DAEDALUS_FULL_STATE_MODULE_PATH}/pam-common"
+daedalus template render-to "${DAEDALUS_CONFIG_MODULE_PATH}/res/pam-common-noninteractive" "${DAEDALUS_FULL_STATE_MODULE_PATH}/pam-common-noninteractive"
+
+daedalus apply sysctl "${DAEDALUS_FULL_STATE_MODULE_PATH}/sysctl.conf"
+daedalus apply security-limits "${DAEDALUS_FULL_STATE_MODULE_PATH}/security_limits.conf"
+daedalus apply systemd "${DAEDALUS_FULL_STATE_MODULE_PATH}/systemd.conf"
+daedalus apply pam-common "${DAEDALUS_FULL_STATE_MODULE_PATH}/pam-common"
+daedalus apply pam-common-noninteractive "${DAEDALUS_FULL_STATE_MODULE_PATH}/pam-common-noninteractive"
+daedalus apply set-hostname "${CORE_HOSTNAME}"
 
 # generate ssh and add to agent
-daedalus ssh keygen git $GIT_EMAIL
+daedalus ssh keygen git "${GIT_EMAIL}"
 daedalus ssh keyadd git
 
-git config --global user.name "$GIT_USERNAME"
-git config --global user.email $GIT_EMAIL
-git remote add update-ssh $GIT_URL
+git config --global user.name "${GIT_USERNAME}"
+git config --global user.email "${GIT_EMAIL}"
+git remote add update-ssh "${GIT_URL}"
 
 daedalus sshconfig add StrictHostKeyChecking no
 daedalus sshconfig add UserKnownHostsFile /dev/null
